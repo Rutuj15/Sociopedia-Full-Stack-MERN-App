afa04aac1fefb3dd8a0cd3719506d5276833578e
Rutuj Palkhedkar <rutuj.palkhedkar@leena.ai>
2025-02-27 00:55:40 +0530

    changed BACKEND_URL to REACT_APP_BACKEND_URL

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -23,7 +23,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
   const patchFriend = async () => {
     const response = await fetch(
-      `${process.env.BACKEND_URL}/users/${_id}/${friendId}`,
+      `${process.env.REACT_APP_BACKEND_URL}/users/${_id}/${friendId}`,
       {
         method: "PATCH",
         headers: {
--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`${process.env.BACKEND_URL}/assets/${image}`}
+        src={`${process.env.REACT_APP_BACKEND_URL}/assets/${image}`}
       />
     </Box>
   );
